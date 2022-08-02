da85d6419d1787b845669c3b9af7c58b4b5ddb22
Davide Morelli <modavide93@gmail.com>
2022-08-02 15:59:07 +0200

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
@@ -6,6 +7,8 @@ import os
 import numpy as np
 import json
 from typing import List, Tuple
+from data.labelmap import label_map
+from numpy.linalg import lstsq
 
 
 class Dataset(data.Dataset):
@@ -102,34 +105,69 @@ class Dataset(data.Dataset):
         im_parse = im_parse.resize((self.width, self.height), Image.NEAREST)
         parse_array = np.array(im_parse)
 
+        parse_shape = (parse_array > 0).astype(np.float32)
+
         parse_head = (parse_array == 1).astype(np.float32) + \
                      (parse_array == 2).astype(np.float32) + \
                      (parse_array == 3).astype(np.float32) + \
                      (parse_array == 11).astype(np.float32)
+
+        parser_mask_fixed = (parse_array == label_map["hair"]).astype(np.float32) + \
+                            (parse_array == label_map["left_shoe"]).astype(np.float32) + \
+                            (parse_array == label_map["right_shoe"]).astype(np.float32) + \
+                            (parse_array == label_map["hat"]).astype(np.float32) + \
+                            (parse_array == label_map["sunglasses"]).astype(np.float32) + \
+                            (parse_array == label_map["scarf"]).astype(np.float32) + \
+                            (parse_array == label_map["bag"]).astype(np.float32)
+
+        parser_mask_changeable = (parse_array == label_map["background"]).astype(np.float32)
 
         arms = (parse_array == 14).astype(np.float32) + (parse_array == 15).astype(np.float32)
 
         if dataroot.split('/')[-1] == 'dresses':
             label_cat = 7
             parse_cloth = (parse_array == 7).astype(np.float32)
+            parse_mask = (parse_array == 7).astype(np.float32) + \
+                         (parse_array == 12).astype(np.float32) + \
+                         (parse_array == 13).astype(np.float32)
+            parser_mask_changeable += np.logical_and(parse_array, np.logical_not(parser_mask_fixed))
 
         elif dataroot.split('/')[-1] == 'upper_body':
             label_cat = 4
             parse_cloth = (parse_array == 4).astype(np.float32)
+            parse_mask = (parse_array == 4).astype(np.float32)
 
+            parser_mask_fixed += (parse_array == label_map["skirt"]).astype(np.float32) + \
+                                 (parse_array == label_map["pants"]).astype(np.float32)
+
+            parser_mask_changeable += np.logical_and(parse_array, np.logical_not(parser_mask_fixed))
         elif dataroot.split('/')[-1] == 'lower_body':
             label_cat = 6
             parse_cloth = (parse_array == 6).astype(np.float32)
+            parse_mask = (parse_array == 6).astype(np.float32) + \
+                         (parse_array == 12).astype(np.float32) + \
+                         (parse_array == 13).astype(np.float32)
+
+            parser_mask_fixed += (parse_array == label_map["upper_clothes"]).astype(np.float32) + \
+                                 (parse_array == 14).astype(np.float32) + \
+                                 (parse_array == 15).astype(np.float32)
+            parser_mask_changeable += np.logical_and(parse_array, np.logical_not(parser_mask_fixed))
 
         parse_head = torch.from_numpy(parse_head)  # [0,1]
         parse_cloth = torch.from_numpy(parse_cloth)   # [0,1]
+        parse_mask = torch.from_numpy(parse_mask)  # [0,1]
+        parser_mask_fixed = torch.from_numpy(parser_mask_fixed)
+        parser_mask_changeable = torch.from_numpy(parser_mask_changeable)
+
+        # dilation
+        parse_without_cloth = np.logical_and(parse_shape, np.logical_not(parse_mask))
+        parse_mask = parse_mask.cpu().numpy()
 
         # Masked cloth
         im_head = im * parse_head - (1 - parse_head)
         im_cloth = im * parse_cloth + (1 - parse_cloth)
 
         # Shape
-        parse_shape = (parse_array > 0).astype(np.float32)
         parse_shape = Image.fromarray((parse_shape * 255).astype(np.uint8))
         parse_shape = parse_shape.resize((self.width // 16, self.height // 16), Image.BILINEAR)
         parse_shape = parse_shape.resize((self.width, self.height), Image.BILINEAR)
@@ -166,6 +204,69 @@ class Dataset(data.Dataset):
         # just for visualization
         im_pose = self.transform2D(im_pose)
 
+        im_arms = Image.new('L', (self.width, self.height))
+        arms_draw = ImageDraw.Draw(im_arms)
+        if dataroot.split('/')[-1] == 'dresses' or dataroot.split('/')[-1] == 'upper_body':
+            with open(os.path.join(dataroot, 'keypoints', pose_name), 'r') as f:
+                data = json.load(f)
+                shoulder_right = np.multiply(tuple(data['keypoints'][2][:2]), self.height / 512.0)
+                shoulder_left = np.multiply(tuple(data['keypoints'][5][:2]), self.height / 512.0)
+                elbow_right = np.multiply(tuple(data['keypoints'][3][:2]), self.height / 512.0)
+                elbow_left = np.multiply(tuple(data['keypoints'][6][:2]), self.height / 512.0)
+                wrist_right = np.multiply(tuple(data['keypoints'][4][:2]), self.height / 512.0)
+                wrist_left = np.multiply(tuple(data['keypoints'][7][:2]), self.height / 512.0)
+                if wrist_right[0] <= 1. and wrist_right[1] <= 1.:
+                    if elbow_right[0] <= 1. and elbow_right[1] <= 1.:
+                        arms_draw.line(np.concatenate((wrist_left, elbow_left, shoulder_left, shoulder_right)).astype(np.uint16).tolist(), 'white', 30, 'curve')
+                    else:
+                        arms_draw.line(np.concatenate((wrist_left, elbow_left, shoulder_left, shoulder_right, elbow_right)).astype(np.uint16).tolist(), 'white', 30, 'curve')
+                elif wrist_left[0] <= 1. and wrist_left[1] <= 1.:
+                    if elbow_left[0] <= 1. and elbow_left[1] <= 1.:
+                        arms_draw.line(np.concatenate((shoulder_left, shoulder_right, elbow_right, wrist_right)).astype(np.uint16).tolist(), 'white', 30, 'curve')
+                    else:
+                        arms_draw.line(np.concatenate((elbow_left, shoulder_left, shoulder_right, elbow_right, wrist_right)).astype(np.uint16).tolist(), 'white', 30, 'curve')
+                else:
+                    arms_draw.line(np.concatenate((wrist_left, elbow_left, shoulder_left, shoulder_right, elbow_right, wrist_right)).astype(np.uint16).tolist(),'white', 30, 'curve')
+
+            if self.args.height > 512:
+                im_arms = cv2.dilate(np.float32(im_arms), np.ones((10, 10), np.uint16), iterations=5)
+            # elif self.args.height > 256:
+            #     im_arms = cv2.dilate(np.float32(im_arms), np.ones((5, 5), np.uint16), iterations=5)
+            hands = np.logical_and(np.logical_not(im_arms), arms)
+            parse_mask += im_arms
+            parser_mask_fixed += hands
+
+        #delete neck
+        parse_head_2 = torch.clone(parse_head)
+        if dataroot.split('/')[-1] == 'dresses' or dataroot.split('/')[-1] == 'upper_body':
+            with open(os.path.join(dataroot, 'keypoints', pose_name), 'r') as f:
+                data = json.load(f)
+                points = []
+                points.append(np.multiply(tuple(data['keypoints'][2][:2]), self.height/512.0))
+                points.append(np.multiply(tuple(data['keypoints'][5][:2]), self.height/512.0))
+                x_coords, y_coords = zip(*points)
+                A = np.vstack([x_coords, np.ones(len(x_coords))]).T
+                m, c = lstsq(A, y_coords, rcond=None)[0]
+                for i in range(parse_array.shape[1]):
+                    y = i * m + c
+                    parse_head_2[int(y - 20*(self.height/512.0)):, i] = 0
+
+        parser_mask_fixed = np.logical_or(parser_mask_fixed, np.array(parse_head_2, dtype=np.uint16))
+        parse_mask += np.logical_or(parse_mask, np.logical_and(np.array(parse_head, dtype=np.uint16), np.logical_not(np.array(parse_head_2, dtype=np.uint16))))
+
+        if self.args.height > 512:
+            parse_mask = cv2.dilate(parse_mask, np.ones((20, 20), np.uint16), iterations=5)
+        # elif self.args.height > 256:
+        #     parse_mask = cv2.dilate(parse_mask, np.ones((10, 10), np.uint16), iterations=5)
+        else:
+            parse_mask = cv2.dilate(parse_mask, np.ones((5, 5), np.uint16), iterations=5)
+        parse_mask = np.logical_and(parser_mask_changeable, np.logical_not(parse_mask))
+        parse_mask_total = np.logical_or(parse_mask, parser_mask_fixed)
+        im_mask = im * parse_mask_total
+        parse_mask_total = parse_mask_total.numpy()
+        parse_mask_total = parse_array * parse_mask_total
+        parse_mask_total = torch.from_numpy(parse_mask_total)
+
         uv = np.load(os.path.join(dataroot, 'dense', im_name.replace('_0.jpg', '_5_uv.npz')))
         uv = uv['uv']
         uv = torch.from_numpy(uv)
@@ -189,6 +290,8 @@ class Dataset(data.Dataset):
             'dense_labels': labels,
             'dense_uv': uv,
             'skeleton': skeleton,
+            'm': im_mask,  # for input
+            'parse_mask_total': parse_mask_total,
         }
 
         return result
